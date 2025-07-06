e70316a0ce976194063f8981b669b341d5544832
S-Bhargavii <136246400+S-Bhargavii@users.noreply.github.com>
2025-07-06 18:51:56 +0800

    Create server.py
    Server code that listens to the jetson publisher

---
+++ server.py
@@ -0,0 +1,13 @@
+from fastapi import FastAPI, WebSocket, WebSocketDisconnect
+
+app = FastAPI()
+
+@app.websocket("/ws")
+async def websocket_endpoint(websocket: WebSocket):
+    await websocket.accept()
+    try:
+        while True:
+            data = await websocket.receive_text()
+            print(f"Recieved from Jetson : {data}")
+    except WebSocketDisconnect:
+        print("Client Disconnected")
