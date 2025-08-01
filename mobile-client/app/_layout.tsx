f15ed8d9ddddebf67b6dd5b6da1ace053b7f4848
S-Bhargavii <bhargavi005@e.ntu.edu.sg>
2025-08-01 10:10:05 +0800

    wip

--- mobile-client/app/_layout.tsx
+++ mobile-client/app/_layout.tsx
@@ -1,5 +1,11 @@
 import { Stack } from "expo-router";
+import "./globals.css";
 
 export default function RootLayout() {
-  return <Stack />;
+  return (
+    <Stack>
+      <Stack.Screen name="splash" options={{title:"Museum Navigator"}} />
+      {/* <Stack.Screen name="role-selection" options={{title:"Choose your role"}}/> */}
+    </Stack>
+  )
 }
---
+++ mobile-client/app/role-selection.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const role_selection = () => {
+  return (
+    <View>
+      <Text>role-selection</Text>
+    </View>
+  )
+}
+
+export default role_selection
