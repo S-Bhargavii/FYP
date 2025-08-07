a35457915d5cd76baf9e3e66807dc84cac6902b6
S-Bhargavii <bhargavi005@e.ntu.edu.sg>
2025-08-07 21:51:14 +0800

    remove map info from constants file

--- backend/constants.py
+++ backend/constants.py
@@ -9,22 +9,3 @@ REDIS_HOST = "localhost"
 REDIS_PORT = 6379
 REDIS_DB = 0
 REDIS_LOCATION_PREFIX = "user_location"
-
-# contains the mapping from the map_id to the corresponding xml file
-MAP_PATH_DICTIONARY = {
-    "map_01" : "map_01.xml"
-}
-
-# contains the mapping from the map_id to the landmarks and ids
-MAP_LANDMARKS_DICTIONARY = {
-    "map_01": {
-        "Join or Die : An Americal Army Takes Shape Boston, 1775": 2,
-        "King George's Statue": 3,
-        "Chain Of States": 4,
-        "Independence Theatre": 5,
-        "The War Begins, 1775": 6,
-        "Boston's Liberty Tree": 7,
-        "Prologue: Tearing Down The King": 8,
-        "The Price Of Victory": 9
-    }
-}
